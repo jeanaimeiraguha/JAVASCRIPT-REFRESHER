3f70623c5f75d13b4201cb26806d45bbc4bf133f
jeanaime-iraguha <jeanaimeiraguha@gmail.com>
2025-01-19 18:24:18 +0200

    js

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 // Getting started with Array
 const Animals=['Cat', 'Cow', 'Goat','Sheep'];
 console.log("The list of animals we have as an array are:",Animals);
+//shopping cart
+const shoppingCart=[
+     'Milk',
+     'Mango',
+     'Tomato',
+     'Potato',
+     'Avocado'
+]
