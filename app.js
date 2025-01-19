7f88fc180052081f7f442546cd5a6435b2484240
jeanaime-iraguha <jeanaimeiraguha@gmail.com>
2025-01-19 18:26:32 +0200

    js

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-// Getting started with Array
-const Animals=['Cat', 'Cow', 'Goat','Sheep'];
-console.log("The list of animals we have as an array are:",Animals);
+// // Getting started with Array
+// const Animals=['Cat', 'Cow', 'Goat','Sheep'];
+// console.log("The list of animals we have as an array are:",Animals);
 //shopping cart
 const shoppingCart=[
      'Milk',
      'Mango',
      'Tomato',
      'Potato',
-     'Avocado'
-]
+     'Avocado', 
+     'Eggs',
+  'Sugar'
+];
+console.log(shoppingCart[0]);
+console.log(shoppingCart[4]);
